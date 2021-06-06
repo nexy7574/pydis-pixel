d20269d12bc000165a622f7860ea655acf07aad3
EEKIM10 <eek@clicksminuteper.net>
2021-06-06 15:03:47 +0100

    Don't install dependencies if creating a venv fails

--- install.sh
+++ install.sh
@@ -2,9 +2,9 @@ echo "Cloning repo..."
 git clone https://github.com/EEKIM10/pydis-pixel
 cd pydis-pixel
 echo "Creating virtual environment..."
-python3 -m venv venv
-. ./venv/bin/activate
-echo "Installing dependencies..."
+python3 -m venv venv && \
+. ./venv/bin/activate && \
+echo "Installing dependencies..." && \
 python3 pip install -r requirements.txt
 echo "Downloading sample image..."
 wget https://cdn.discordapp.com/embed/avatars/0.png -O sample-image.png
