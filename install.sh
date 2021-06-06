fe764fe5e910ed0622dfd63f99a775c24bb05566
EEKIM10 <eek@clicksminuteper.net>
2021-06-06 15:02:10 +0100

    Let the user know the script isn't frozen

--- install.sh
+++ install.sh
@@ -1,7 +1,11 @@
+echo "Cloning repo..."
 git clone https://github.com/EEKIM10/pydis-pixel
 cd pydis-pixel
+echo "Creating virtual environment..."
 python3 -m venv venv
 . ./venv/bin/activate
+echo "Installing dependencies..."
 python3 pip install -r requirements.txt
+echo "Downloading sample image..."
 wget https://cdn.discordapp.com/embed/avatars/0.png -O sample-image.png
-python3 main.py
+echo "Installed! Please see python3 main.py --help, and get started!"
