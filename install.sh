ec13eb3abcb4175031f272366349c6a685fa818e
EEKIM10 <eek@clicksminuteper.net>
2021-06-06 15:06:12 +0100

    Output is for nerds

--- install.sh
+++ install.sh
@@ -1,11 +1,11 @@
 echo "Cloning repo..."
-git clone https://github.com/EEKIM10/pydis-pixel
+git clone https://github.com/EEKIM10/pydis-pixel > /dev/null
 cd pydis-pixel
 echo "Creating virtual environment..."
-python3 -m venv venv && \
+python3 -m venv venv > /dev/null&& \
 . ./venv/bin/activate && \
 echo "Installing dependencies..." && \
-python3 pip install -r requirements.txt
+python3 pip install -r requirements.txt > /dev/null
 echo "Downloading sample image..."
 wget https://cdn.discordapp.com/embed/avatars/0.png -O sample-image.png
 echo "Installed! Please see python3 main.py --help, and get started!"
