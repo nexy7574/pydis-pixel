f490687c12e1ab0e91ac9c19d03795adc9cfcb38
eek#7574 <samueljohnnicholson@gmail.com>
2021-06-11 12:13:25 +0100

    Fix install.sh not calling pip properly

--- install.sh
+++ install.sh
@@ -5,7 +5,7 @@ echo "Creating virtual environment..."
 python3 -m venv venv > /dev/null&& \
 . ./venv/bin/activate && \
 echo "Installing dependencies..." && \
-python3 pip install -r requirements.txt > /dev/null
+python3 -m pip install -r requirements.txt > /dev/null
 echo "Downloading sample image..."
 wget https://cdn.discordapp.com/embed/avatars/0.png -O sample-image.png
 echo "Installed! Please see python3 main.py --help, and get started!"
