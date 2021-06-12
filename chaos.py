517aad332eebaa6db368a78b4c0c041457334133
EEKIM10 <eek@clicksminuteper.net>
2021-06-12 07:06:12 +0100

    For when you wanna be annoying

---
+++ chaos.py
@@ -0,0 +1,22 @@
+import os
+
+from lib import api
+from random import randint
+
+width, height = api.get_size()
+colour = os.getenv("COLOUR", "RANDOMISE")
+
+iteration = 0
+while True:
+    iteration += 1
+    if not iteration % 300:
+        width, height = api.get_size()
+    x = randint(0, width)
+    y = randint(0, height)
+    if colour.upper() == "RANDOMISE":
+        col = randint(0x0, 0xFFFFFF)
+    else:
+        col = int(colour, 16)
+    h = hex(col)[2:]
+    print(f"[CURSOR] Setting ({x}, {y}) to #{h}")
+    api.set_pixel(x, y, h)
