d6f6946fdcad62f55623aa244fb8f3b9ca489299
TymanWasTaken <tyman@tyman.tech>
2021-06-16 09:37:43 -0500

    Add download and exit argument

--- lib/cli.py
+++ lib/cli.py
@@ -138,8 +138,16 @@ parser.add_argument(
     help="Shows the version and exits."
 )
 parser.add_argument(
-    "--download-canvas",
+    "--download-canvas-exit",
     "-D",
+    action="store_true",
+    default=False,
+    help="Downloads the canvas to canvas.png and exits",
+    dest="download_exit"
+)
+parser.add_argument(
+    "--download-canvas",
+    "-d",
     action="store_true",
     default=False,
     help="Downloads the canvas to canvas.png",
